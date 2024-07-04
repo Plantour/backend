b8c7a19f1840d2cb1ff7cb605ba0b5434499adca
qnelldo <131656049+qnelldo@users.noreply.github.com>
2024-07-04 12:09:06 +0900

    - 최초 세팅 테스트

---
+++ src/main/java/com/qnelldo/plantour/PlantourApplication.java
@@ -0,0 +1,13 @@
+package com.qnelldo.plantour;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlantourApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlantourApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/qnelldo/plantour/PlantourApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qnelldo.plantour;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlantourApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
