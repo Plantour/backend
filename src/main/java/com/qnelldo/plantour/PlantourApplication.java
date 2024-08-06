4f8123cfd62b51e3ec5cfac8b9c809b5e64d5905
qnelldo <131656049+qnelldo@users.noreply.github.com>
2024-08-07 04:37:02 +0900

    - 배포 실패 픽스 - 테스트 코드 추가

--- src/main/java/com/qnelldo/plantour/PlantourApplication.java
+++ src/main/java/com/qnelldo/plantour/PlantourApplication.java
@@ -2,12 +2,12 @@ package com.qnelldo.plantour;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories
 public class PlantourApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(PlantourApplication.class, args);
     }
-
 }
