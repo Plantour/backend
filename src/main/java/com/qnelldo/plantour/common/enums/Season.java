d962fe09d1e86714217f0f6bf2fb8d091541002c
qnelldo <131656049+qnelldo@users.noreply.github.com>
2024-08-22 22:50:13 +0900

    퀘스트 페이지 SEASON 정보 언어 별 데이터 제공

--- src/main/java/com/qnelldo/plantour/common/enums/Season.java
+++ src/main/java/com/qnelldo/plantour/common/enums/Season.java
@@ -2,16 +2,19 @@ package com.qnelldo.plantour.common.enums;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 
 public enum Season {
-    SPRING(3, 4, 5),
-    SUMMER(6, 7, 8),
-    AUTUMN(9, 10, 11),
-    WINTER(12, 1, 2);
+    SPRING(Map.of("ENG", "SPRING", "KOR", "봄"), 3, 4, 5),
+    SUMMER(Map.of("ENG", "SUMMER", "KOR", "여름"), 6, 7, 8),
+    AUTUMN(Map.of("ENG", "AUTUMN", "KOR", "가을"), 9, 10, 11),
+    WINTER(Map.of("ENG", "WINTER", "KOR", "겨울"), 12, 1, 2);
 
     private final List<Integer> months;
+    private final Map<String, String> names;
 
-    Season(Integer... months) {
+    Season(Map<String, String> names, Integer... months) {
+        this.names = names;
         this.months = Arrays.asList(months);
     }
 
@@ -21,4 +24,9 @@ public enum Season {
                 .findFirst()
                 .orElseThrow(() -> new IllegalArgumentException("Invalid month"));
     }
+
+    public String getName(String languageCode) {
+        return names.getOrDefault(languageCode, names.get("ENG")); // 기본값으로 영어를 사용
+    }
+
 }
